b361a1434daeee25dbed32d739734185898e5d00
Luis Benitez <luisbttf@gmail.com>
2016-08-26 21:08:49 -0500

    videos

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,5 +1,21 @@
-$(function(){
-    $('.video1').click(function(){
-        $('.overlay').css('display','flex');
+$(document).ready(function(){
+    $("#video1").click(function(evento){
+        $('.overlay').css('display', 'flex');
+        $('.modal1').css('display', 'block');
     });
-}
+    $("#video2").click(function(evento){
+        $('.overlay').css('display', 'flex');
+        $('.modal2').css('display', 'block');
+    });
+    $("#video3").click(function(evento){
+        $('.overlay').css('display', 'flex');
+        $('.modal3').css('display', 'block');
+    });
+
+    $(".cerrar").click(function(evento){
+        $('.overlay').css('display', 'none');
+        $('.modal1').css('display', 'none');
+        $('.modal2').css('display', 'none');
+        $('.modal3').css('display', 'none');
+    });
+});
