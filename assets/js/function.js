059c7a46e273330225d0c30c96c991450add76c9
Luis Benitez <luisbttf@gmail.com>
2016-08-28 19:24:44 -0500

    responsive 1

--- assets/js/function.js
+++ assets/js/function.js
@@ -12,10 +12,41 @@ $(document).ready(function(){
         $('.modal3').css('display', 'block');
     });
 
+    $(".hamburger").click(function(evento){
+        $('.overlay').css('display', 'flex');
+        $('.menu-responsive').css('display', 'block');
+    });
+
     $(".cerrar").click(function(evento){
         $('.overlay').css('display', 'none');
         $('.modal1').css('display', 'none');
         $('.modal2').css('display', 'none');
         $('.modal3').css('display', 'none');
     });
+
+    $(".overlay").click(function(evento){
+        $('.overlay').css('display', 'none');
+        $('.modal1').css('display', 'none');
+        $('.modal2').css('display', 'none');
+        $('.modal3').css('display', 'none');
+        $('.menu-responsive').css('display', 'none');
+    });
+
+
+
+
+    $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+  
 });
